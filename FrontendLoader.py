fb501e00e9175ba9d9d5543c461a21d2d6ebf335
aziznal <abodenaal@gmail.com>
2020-07-12 00:01:26 +0300

    Create first connection with frontend gui

--- FrontendLoader.py
+++ FrontendLoader.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
 import sys
 
 
@@ -7,9 +8,38 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('static_gui.ui', self)
 
+        self.__load_everything()
+        self.__test_load_button()
+
+    def __load_all_buttons(self):
+        pass
+
+    def __load_all_labels(self):
+        pass
+
+    def __load_all_text_entry_boxes(self):
+        pass
+
+    def __load_dropdown_menus(self):
+        pass
+    
+    def __load_everything(self):
+        self.__load_all_buttons()
+        self.__load_all_labels()
+        self.__load_all_text_entry_boxes()
+        self.__load_dropdown_menus()
+
+    def __test_load_button(self):
+        self.test_button:QObject = self.findChild(QPushButton, 'createButton')
+        self.test_button.clicked.connect(self.__say_hi)
+        self.test_button
+
+    def __say_hi(self):
+        print("Hello World!")
+
 
 def start_program():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = Ui()
     window.show()
     app.exec_()
