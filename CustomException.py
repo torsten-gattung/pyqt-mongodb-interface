b15fead50683a9114ed8fa6bbe51625c486b2882
aziznal <abodenaal@gmail.com>
2020-07-15 00:59:45 +0300

    Create class for custom exceptions

---
+++ CustomException.py
@@ -0,0 +1,10 @@
+
+class GeneralException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class UnimplementedMethodException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
